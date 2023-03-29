fcf9aca77978317636336281a6d38e54a608c071
unknown <jacobmarcosoliver@gmail.com>
2023-03-29 23:42:20 +0430

    first commit

---
+++ libs/libs2/libs3/app.js
@@ -0,0 +1,19 @@
+// let name = 'ali'
+// let names = ['alireza', 'mehdi', 'hoseyn', name]
+// let tedad = names.length
+// // let s = `tedad esm ha: ${tedad}`
+// // console.log(s)
+// // console.log(`1: ${names[0]}`)
+// // console.log(`2: ${names[1]}`)
+// // console.log(`3: ${names[2]}`)
+// // console.log(`4: ${names[3]}`)
+
+// for (let i = 0; i < tedad; i = i + 1) {
+//     console.log(names[i])
+// }
+
+// console.log("Finish!")
+// console.log("Finish!")
+
+
+console.log("alireza");
